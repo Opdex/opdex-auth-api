4e3ad1dedfd0512317d1174cdbaabe47b649a5d0
drmathias <adam@opdex.com>
2022-04-22 16:56:40 +0100

    Change expires_in from milliseconds to seconds

--- src/Opdex.Auth.Api/Models/TokenResponseBody.cs
+++ src/Opdex.Auth.Api/Models/TokenResponseBody.cs
@@ -4,6 +4,6 @@ namespace Opdex.Auth.Api.Models;
 
 public record TokenResponseBody(string AccessToken, string RefreshToken)
 {
-    public ulong ExpiresIn => (ulong)JwtIssuer.TokenLifetime.TotalMilliseconds;
+    public ulong ExpiresIn => (ulong)JwtIssuer.TokenLifetime.TotalSeconds;
     public string TokenType => "bearer";
 }
